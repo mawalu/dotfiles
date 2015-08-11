441db76f4351a63d62ae6464241b2c4fef764aad
mawalu <web@mawalabs.de>
2015-08-11 11:03:21 +0200

    Force symlinks

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 INSTALL=(.vim .vimrc .zshrc .tmuxinator)
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 for i in ${INSTALL[*]} ; do
-    ln -s "$(pwd)/$i" $HOME
+    ln -s --force "$(pwd)/$i" $HOME
 done
