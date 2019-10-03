9fb86b131829648885d476637ae3fa0ebc90f4d3
mawalu <web@mawalabs.de>
2019-10-03 17:44:27 +0200

    Update dotfiles

--- scripts/bin/rofi.sh
+++ scripts/bin/rofi.sh
@@ -2,6 +2,6 @@
 if [[ -a ~/.is-laptop ]]; then
   rofi -combi-modi drun -show combi -modi "combi,Playnext:play_next.sh"
 else
-  rofi -combi-modi drun -monitor DVI-I-1 -show combi -modi "combi,Playnext:play_next.sh"
+  rofi -combi-modi drun -show combi -modi "combi,Playnext:play_next.sh"
 fi
 
